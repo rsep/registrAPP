450f1fc36f249c10bc0170ce5af7cbf4a5ebbe83
rsep <78937557+rsep@users.noreply.github.com>
2021-09-11 00:03:20 -0300

    añadir pages anidadas en carpeta (orden)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,15 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'recuperacion',
+    loadChildren: () => import('./pages/recuperacion/recuperacion.module').then( m => m.RecuperacionPageModule)
+  },
+
 ];
 
 @NgModule({
