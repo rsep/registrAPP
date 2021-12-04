af8b5f758ff843f25a230c0e4abb3f065237067e
CamiloFloresDuoc <cm.flores@duocuc.cl>
2021-12-03 23:54:53 -0300

    mail automatico le camilo

--- src/app/interfaces/iattend.ts
+++ src/app/interfaces/iattend.ts
@@ -1,10 +1,13 @@
 export interface IAttend {
     id: number,
     userId: number,
+    asignatura: string,
     ramo: {
         sigla: string,
         seccion: string
     },
+    docente: string,
+    correo: string,
     fecha: string,
     hora: string
 }
